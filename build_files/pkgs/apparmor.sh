79f74f2b72d6d2ea7119144f7e0cf2cb517e0891
JumpyVi <62212220+jumpyvi@users.noreply.github.com>
2026-05-01 14:58:54 -0400

    Disable apparmor.d

--- build_files/pkgs/apparmor.sh
+++ build_files/pkgs/apparmor.sh
@@ -4,16 +4,16 @@ set -ouex pipefail
 
 apt-get install -y lsb-release wget gnupg
 
-wget -qO - https://pkg.pujol.io/debian/gpgkey \
-    | gpg --dearmor \
-    | tee /usr/share/keyrings/roddhjav.gpg >/dev/null
-cat <<-EOF | tee /etc/apt/sources.list.d/roddhjav.sources
-Types: deb
-URIs: https://pkg.pujol.io/debian/repo
-Suites: trixie
-Components: main
-Signed-By: /usr/share/keyrings/roddhjav.gpg
-EOF
+#wget -qO - https://pkg.pujol.io/debian/gpgkey \
+#    | gpg --dearmor \
+#    | tee /usr/share/keyrings/roddhjav.gpg >/dev/null
+#cat <<-EOF | tee /etc/apt/sources.list.d/roddhjav.sources
+#Types: deb
+#URIs: https://pkg.pujol.io/debian/repo
+#Suites: trixie
+#Components: main
+#Signed-By: /usr/share/keyrings/roddhjav.gpg
+#EOF
 
 apt-get update
 
@@ -27,8 +27,8 @@ mkdir -p /etc/apparmor/earlypolicy/
 printf 'kargs = ["apparmor=1", "lsm=lockdown,yama,apparmor"]\n' > /usr/lib/bootc/kargs.d/10-apparmor.toml
 printf '[Unit]\nConditionSecurity=\n' > /etc/systemd/system/apparmor.service.d/override.conf
 
-echo 'write-cache' | tee -a /etc/apparmor/parser.conf
-echo 'cache-loc /etc/apparmor/earlypolicy/' | tee -a /etc/apparmor/parser.conf
-echo 'Optimize=compress-fast' | tee -a /etc/apparmor/parser.conf
+#echo 'write-cache' | tee -a /etc/apparmor/parser.conf
+#echo 'cache-loc /etc/apparmor/earlypolicy/' | tee -a /etc/apparmor/parser.conf
+#echo 'Optimize=compress-fast' | tee -a /etc/apparmor/parser.conf
 
 systemctl enable apparmor.service
