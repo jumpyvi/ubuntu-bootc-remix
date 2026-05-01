5e9c633617f6bc315bae5d2fa1769835b06201f2
JumpyVi <62212220+jumpyvi@users.noreply.github.com>
2026-05-01 16:11:33 -0400

    Update apparmor.sh

--- build_files/pkgs/apparmor.sh
+++ build_files/pkgs/apparmor.sh
@@ -13,9 +13,9 @@ mkdir -p /usr/lib/bootc/kargs.d/
 mkdir -p /etc/systemd/system/apparmor.service.d/
 mkdir -p /etc/apparmor/earlypolicy/
 
-mkdir -p /usr/etc/
+mkdir -p /usr/local/etc/
 
-cp -r /etc/apparmor* /usr/etc/
+cp -r /etc/apparmor* /usr/local/etc/
 
 printf 'kargs = ["apparmor=1", "lsm=lockdown,yama,apparmor"]\n' > /usr/lib/bootc/kargs.d/10-apparmor.toml
 printf '[Unit]\nConditionSecurity=\n' > /etc/systemd/system/apparmor.service.d/override.conf
